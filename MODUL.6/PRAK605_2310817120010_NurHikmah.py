a622e95da67800997a6e05032b06d02f2933fee4
nrhikmahhh99 <144782363+nrhikmahhh99@users.noreply.github.com>
2023-12-16 21:20:36 +0800

    PRAK605_2310817120010_NurHikmah.py

--- MODUL.6/PRAK605_2310817120010_NurHikmah.py
+++ MODUL.6/PRAK605_2310817120010_NurHikmah.py
@@ -1,15 +1,12 @@
-# Membaca ordo matriks
 print("Input")
 n = int(input())
 
-# Membaca matriks A
 print("Matriks A")
 matriksA = []
 for i in range(n):
     row = list(map(int, input().split()))
     matriksA.append(row)
 
-# Membaca matriks B
 print("Matriks B")
 matriksB = []
 for i in range(n):
@@ -23,7 +20,7 @@ for i in range(n):
         for k in range(n):
             hasil[i][j] += matriksA[i][k] * matriksB[k][j]
 
-# Menampilkan hasil perkalian matriks
+# hasil perkalian matriks
 print("\nOutput\nMatriks AXB")
 for i in range(n):
     for j in range(n):
